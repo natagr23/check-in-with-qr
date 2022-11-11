5347dfb2dcce64eda356b82fd6ce80a3f83ac95f
natagr23 <nata_garcia23@hotmail.com>
2022-11-11 14:57:23 -0500

    adding qr examples

--- src/Pages/AdminAccount.js
+++ src/Pages/AdminAccount.js
@@ -1,12 +1,34 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button, Box } from '@mui/material';
 import { Context } from '../Pages/Context';
-import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Locations from './Locations';
+
+import { QRCodeSVG } from 'qrcode.react';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 export default function AdminAccount() {
   const ctx = useContext(Context);
   let navigate = useNavigate();
+
+  const [url, setUrl] = useState(
+    'https://picturesofpeoplescanningqrcodes.tumblr.com/'
+  );
+  const call_Url = (product) => {
+    setUrl(product.url);
+  };
+
   const handleLogout = () => {
     navigate('/');
     ctx.currentUser = 1;
@@ -30,9 +52,35 @@ export default function AdminAccount() {
         <div> Account Admin</div>
         <p> Account...Admin</p>
 
-        <Stack direction="row" spacing={2}>
-          <Button>Create Products</Button>
-        </Stack>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={4}>
+              <Item>Products List</Item>
+              <Locations url={url} call_Url={call_Url} />
+            </Grid>
+            <Grid item xs={8}>
+              <Item>QR</Item>
+              <Item>
+                <QRCodeSVG
+                  value={url}
+                  size={128}
+                  bgColor={'#ffffff'}
+                  fgColor={'#000000'}
+                  level={'L'}
+                  includeMargin={false}
+                  imageSettings={{
+                    src: 'https://static.zpao.com/favicon.png',
+                    x: undefined,
+                    y: undefined,
+                    height: 24,
+                    width: 24,
+                    excavate: true,
+                  }}
+                />
+              </Item>
+            </Grid>
+          </Grid>
+        </Box>
         <Button
           sx={{ marginLeft: '800px' }}
           variant="contained"
---
+++ src/Pages/Locations.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+
+const products = [
+  {
+    id: 1,
+    name: 'Manzanas',
+    url: 'https://es.wikipedia.org/wiki/Manzana',
+  },
+  {
+    id: 2,
+    name: 'Peras',
+    url: 'https://es.wikipedia.org/wiki/Pera',
+  },
+  {
+    id: 3,
+    name: 'Limones',
+    url: 'https://es.wikipedia.org/wiki/Lima_(fruta)',
+  },
+  {
+    id: 4,
+    name: 'Bananos',
+    url: 'https://es.wikipedia.org/wiki/Musa_%C3%97_paradisiaca',
+  },
+  {
+    id: 5,
+    name: 'Melocotones',
+    url: 'https://es.wikipedia.org/wiki/Melocot%C3%B3n_de_Calanda',
+  },
+];
+
+export default function Locations({ call_Url, url }) {
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Stack spacing={1}>
+        {products.map((product) => {
+          return (
+            <Button
+              key={product.id}
+              onClick={() => {
+                call_Url(product);
+              }}
+              variant="contained"
+              color="success"
+            >
+              {product.name}
+            </Button>
+          );
+        })}
+        <div>{url}</div>
+      </Stack>
+    </Box>
+  );
+}
